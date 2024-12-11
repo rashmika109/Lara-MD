4a369ce61ce2266b7ef9c8431b6e5d4e86007b8e
rashmika109 <153777824+rashmika109@users.noreply.github.com>
2024-12-11 08:51:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "SJpARKyb#Iprzc3Ptb2_4kYZjxkfHVrY8lQdoMAncPAW4QtFbmp0", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
